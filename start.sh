ed498bf3c297691dccdaa7a169cf21795ed45588
Dumas99 <shelpyakovd@bk.ru>
2020-12-25 18:23:25 +0500

    start.sh edit

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 sudo apt-get install software-properties-common
 sudo apt-add-repository universe
 sudo apt-get update
-sudo apt-get install python-pip
+sudo apt-get install python3-pip
 pip install pytelegrambotapi
 python bot.py
